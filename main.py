a915acf3220ee99e14836659d0f5d5ad89809c03
ProGamer368 <62499031+ProGamer368@users.noreply.github.com>
2020-12-10 10:23:25 +0400

    Added files via upload

--- main.py
+++ main.py
@@ -4,6 +4,16 @@ import discord
 intents = discord.Intents.all()
 client = commands.Bot(command_prefix = ".", intents = intents)
 
+try:
+	for filename in os.listdir('./cogs'):
+		if filename.endswith('.py'):
+			client.load_extension(f"cogs.{filename[:-3]}")
+except Exception as e:
+	print("Cogs error: Cannot load cogs")
+	print("\033[5;37;40m\033[1;33;40mWARNING\033[1;33;40m\033[0;37;40m", end=' ')
+	print("Functionality limited!\n")
+	print(f"exception thrown:\n{e}")
+
 @client.event
 async def on_ready():
   print("The bot is ready!")
@@ -13,4 +23,4 @@ async def ping(ctx, message=True):
     message = await ctx.send("Pinging...")
     await message.edit(content = f"Pong! `{round(client.latency * 1000)}ms`")	
 
-client.run("token")
+client.run("TOKEN")
